ccc0afd97fa13b45f85550f9baa22c3361397e6d
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-13 15:37:47 -0500

    add service get post by pages

--- dist/routes/post.js
+++ dist/routes/post.js
@@ -13,17 +13,36 @@ const express_1 = require("express");
 const autenticacion_1 = require("../middlewares/autenticacion");
 const post_model_1 = require("../models/post.model");
 const postRoutes = (0, express_1.Router)();
-postRoutes.post('/', [autenticacion_1.verificaToken], (req, res) => {
+//obtener post paginados
+postRoutes.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10;
+    const posts = yield post_model_1.Post.find()
+        .sort({ _id: -1 })
+        .limit(10)
+        .populate("usuario", "-password")
+        .skip(skip)
+        .exec();
+    res.json({
+        ok: true,
+        pagina,
+        posts,
+    });
+}));
+//Crear Post
+postRoutes.post("/", [autenticacion_1.verificaToken], (req, res) => {
     const body = req.body;
     body.usuario = req.usuario._id;
-    post_model_1.Post.create(body).then((postDB) => __awaiter(void 0, void 0, void 0, function* () {
-        yield postDB.populate('usuario', '-password');
+    post_model_1.Post.create(body)
+        .then((postDB) => __awaiter(void 0, void 0, void 0, function* () {
+        yield postDB.populate("usuario", "-password");
         res.json({
             ok: true,
-            postDB
+            postDB,
         });
     }))
-        .catch(err => {
+        .catch((err) => {
         res.json(err);
     });
 });
--- routes/post.ts
+++ routes/post.ts
@@ -1,21 +1,41 @@
 import { Router, Response } from "express";
-import { verificaToken } from '../middlewares/autenticacion';
-import { Post } from '../models/post.model';
+import { verificaToken } from "../middlewares/autenticacion";
+import { Post } from "../models/post.model";
 
 const postRoutes = Router();
 
-postRoutes.post('/', [ verificaToken ], (req: any, res: Response) => {
-    const body = req.body;
-    body.usuario = req.usuario._id;
-    Post.create( body ).then( async postDB => {
-        await postDB.populate('usuario', '-password');
-        res.json({
-            ok: true,
-            postDB
-        });
+//obtener post paginados
+postRoutes.get("/", async (req: any, res: Response) => {
+  let pagina = Number(req.query.pagina) || 1;
+  let skip = pagina - 1;
+  skip = skip * 10;
+  const posts = await Post.find()
+    .sort({ _id: -1 })
+    .limit(10)
+    .populate("usuario", "-password")
+    .skip( skip )
+    .exec();
+  res.json({
+    ok: true,
+    pagina,
+    posts,
+  });
+});
+
+//Crear Post
+postRoutes.post("/", [verificaToken], (req: any, res: Response) => {
+  const body = req.body;
+  body.usuario = req.usuario._id;
+  Post.create(body)
+    .then(async (postDB) => {
+      await postDB.populate("usuario", "-password");
+      res.json({
+        ok: true,
+        postDB,
+      });
     })
-    .catch( err => {
-        res.json(err);
+    .catch((err) => {
+      res.json(err);
     });
 });
 
